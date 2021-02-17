51ffa3b9354e1c09e2117c497dc710945607b3ea
jonlima2018 <jonathan_martins_lima@yahoo.com.br>
2021-02-17 17:27:04 -0300

    fix: modify heigth and margin

--- src/pages/Home/Styled.js
+++ src/pages/Home/Styled.js
@@ -3,20 +3,18 @@ import teams from "../../assets/team.jpg";
 
 const Container = styled.div`
 	display: flex;
-	height: 100vh;
 	width: ${({ theme }) => theme.screenWidth.w1};
-	margin: 0 auto;
-
-	background: url(${teams});
+	margin: 50px auto;
+	align-items: center;
+	background-image: url(${teams});
 	background-repeat: no-repeat;
 	background-size: 750px 450px;
 	background-position: right center;
-	align-items: center;
 
 	.home__containt {
-		margin-top: -130px;
-		margin-left: 120px;
+		height: 460px;
 		width: 430px;
+		margin-left: 120px;
 
 		h1,
 		p {
