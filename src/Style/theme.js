d6339c0739ae8a98eebf040181ce3c48699a3793
jonlima2018 <jonathan_martins_lima@yahoo.com.br>
2021-02-17 15:31:18 -0300

    feat: add variables css

--- src/Style/theme.js
+++ src/Style/theme.js
@@ -1,10 +1,21 @@
 const theme = {
 	colors: {
 		orange: "#D2872C",
+		darkOrange: "#DD7700",
 		gray: "#766F68",
+		white: "#ffffff",
 	},
 	fonts: {
 		primary: "'Roboto', sans-serif",
+	},
+	fontWeight: {
+		normal: 400,
+		bold: 600,
+	},
+	fontSize: {
+		f1: "24px",
+		// f2: "18px",
+		f3: "60px",
 	},
 };
 
