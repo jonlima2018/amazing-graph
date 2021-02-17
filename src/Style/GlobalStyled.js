33582a495cdbd9ebcff07bc02120ec985e244ba7
jonlima2018 <jonathan_martins_lima@yahoo.com.br>
2021-02-17 15:33:49 -0300

    feat: create button component

--- src/Style/GlobalStyled.js
+++ src/Style/GlobalStyled.js
@@ -9,7 +9,28 @@ const GlobalStyled = createGlobalStyle`
 	list-style: none;
     border: 0;
     outline: none;
+    box-sizing:border-box;
 }
+
+.btn{
+    font-family: ${({ theme }) => theme.fonts.primary};
+    background-color: ${({ theme }) => theme.colors.orange};
+    color: ${({ theme }) => theme.colors.white};
+    padding: 10px;
+    width: 220px;
+    border-radius: 8px;
+    font-size: ${({ theme }) => theme.fontSize.f1};
+    font-weight: ${({ theme }) => theme.fontWeight.bold};
+    transition: background-color 0.4s;
+
+    &:hover{
+        cursor:pointer;
+        background-color: ${({ theme }) => theme.colors.darkOrange};
+    
+    }
+}
+
+
 
 `;
 
