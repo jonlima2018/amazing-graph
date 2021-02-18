4b661720553bcb907c828c4cb7aacdd61d6310a3
jonlima2018 <jonathan_martins_lima@yahoo.com.br>
2021-02-17 21:14:08 -0300

    fix: fixed align menu items

--- src/components/Header/Styled.js
+++ src/components/Header/Styled.js
@@ -4,6 +4,7 @@ const SHeader = styled.header`
 	width: ${({ theme }) => theme.screenWidth.w1};
 	display: flex;
 	justify-content: space-between;
+	align-items: center;
 	margin: 0 auto;
 	padding: 20px 40px 20px 80px;
 
